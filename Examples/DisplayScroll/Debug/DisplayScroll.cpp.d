b7e1cb2e51c8effe9ac73cc05f112e82b952c9a6
Tomáš Moravec <moravec-tomas@seznam.cz>
2018-06-09 14:19:04 +0200

    Fixed last char was not saving

---
+++ Examples/DisplayScroll/Debug/DisplayScroll.cpp.d
@@ -0,0 +1,14 @@
+C:\Users\morav\AppData\Local\Temp\VMBuilds\DisplayScroll\uno\Debug\DisplayScroll.cpp.o: \
+ C:\Users\morav\AppData\Local\Temp\VMBuilds\DisplayScroll\uno\Debug\DisplayScroll.cpp \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Programs\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\morav\Documents\Arduino\libraries\S1D13700/S1D13700.h
--- Examples/DisplayScroll/DisplayScroll.ino
+++ Examples/DisplayScroll/DisplayScroll.ino
@@ -1,92 +1,128 @@
 #include <S1D13700.h>
 
 S1D13700 glcd;
-const int displayLines = 30;
-const int charsPerLine = 40;
-int line = 0;
+const int displayLines = 5;
+const int charsPerLine = 40;  
+int currentLine = 0;
 char lineNumber;
-char data[displayLines][charsPerLine];
+char data[displayLines][charsPerLine + 1];  // Plus EOL
 bool scroll = false;
 
 void setup()
 {
-	// Init communication
+	// Allocate two-dim memory
+	for (int i = 0; i < displayLines; i++) {
+		for (int j = 0; j < charsPerLine; j++) {
+			data[i][j] = ' ';
+		}
+	}
+
+	// Init display communication
 	glcd.pins.d0 = 14;
 	glcd.pins.d1 = 15;
 	glcd.initLCD();
-	Serial.begin(9600);
 
 	// Clear display
 	glcd.clearGraphic();
 	glcd.clearText();
+
+	// Init serial communication
+	glcd.textGoTo(0, 0);
+	glcd.writeText("Connection established!");
+	Serial.begin(9600);
+	Serial.println("Connection established!");
 }
 
 void loop()
 {
 	// If new message arrives
 	if (Serial.available()) {
-
-		// Save data
-		saveData(Serial.readString());
-
-		// Write on screen
-		glcd.clearText();
-		for (int i = 0; i < displayLines; i++) {
-			glcd.textGoTo(0, lineNumber + i);
-			glcd.writeText(&data[i][0u]);
-		}
-		Serial.setTimeout(10);
-
-		// Show memory into console
-		for (int i = 0; i < displayLines; i++) {
-			Serial.print(i);
-			Serial.print(": ");
-			Serial.println(data[i]);
-		}
-		Serial.println("\n----------");
+		saveData(Serial.readString());  // Read and save data
+		writeOnScreen();  // Write on screen
+		printMemoryOnSerial(); // Print memory into console (serial)
 	}
 }
 
+// Print memory into console (serial)
+void printMemoryOnSerial(){
+	for (int i = 0; i < displayLines; i++) {
+		Serial.print(i);
+		Serial.print(": ");
+		Serial.println(data[i]);
+	}
+	Serial.println("\n----------");
+}
+
+// Print memory into console char by char (serial)
+void printMemoryOnSerialDetail() {
+	for (int i = 0; i < displayLines; i++) {
+		Serial.print(i);
+		Serial.print(": ");
+		Serial.println(data[i]);
+
+		for (int j = 0; j < charsPerLine; j++) {
+			Serial.print("\t[");
+			Serial.print(j);
+			Serial.print("]:\t");
+			Serial.println(data[i][j]);
+		}
+		Serial.println();
+	}
+	Serial.println("\n----------");
+}
+
+// Write memory on screen
+void writeOnScreen() {
+	glcd.clearText();
+	for (int i = 0; i < displayLines; i++) {
+		glcd.textGoTo(0, lineNumber + i);
+		glcd.writeText(&data[i][0u]);
+	}
+	Serial.setTimeout(10);
+}
+
 // Save data in two-dimensional array
-void saveData(String text) {
+void saveData(String message) {
 
 	// Prepare data and display
-	text.trim();  // Delete whitespaces
+	message.trim();  // Delete whitespaces
+	Serial.print("Posledni znak: ");
+	Serial.println(message.charAt(charsPerLine - 1));
 	if (scroll) shiftDataVertically();  // If screen full -> scroll
-	
+
 	// Save also overflow lines
-	if (text.length() > charsPerLine) {
+	if (message.length() > charsPerLine) {
 
 		// Count number of overflow lines
-		float overflow = (float)text.length() / (float)charsPerLine;
+		float overflow = (float)message.length() / (float)charsPerLine;
 		int overflowLines = ((overflow == (int)overflow) ? overflow : overflow + 1) - 1;
 
 		// Save first line
-		text.toCharArray(data[line], charsPerLine);  // Save data in array
+		message.toCharArray(data[currentLine], charsPerLine + 1);  // Save data in array
 
 		// Save overflow lines
 		for (int i = 0; i < overflowLines; i++) {
-			if (scroll) shiftDataVertically(); else line++;  // Scroll if display full, move cursor if not
-			text.remove(0, charsPerLine);  // Cut already saved part
-			text.toCharArray(data[line], charsPerLine);  // Save data in array
+			if (scroll) shiftDataVertically(); else currentLine++;  // Scroll if display full, move cursor if not
+			message.remove(0, charsPerLine);  // Cut already saved part
+			message.toCharArray(data[currentLine], charsPerLine + 1);  // Save data in array
 		}
 	}
 
 	// For only one line
 	else {
-		text.toCharArray(data[line], charsPerLine+1);  // Save data in array
+		message.toCharArray(data[currentLine], charsPerLine + 1);  // Save data in array
 	}
 
 	// Check if display is full
 	if (!scroll) {
-		line++;  // Increase line number for next time
-		if (line + 1 == displayLines) scroll = true;  // If screen full, enable scroll
+		currentLine++;  // Increase line number for next time
+		if (currentLine + 1 == displayLines) scroll = true;  // If screen full, enable scroll
 	}
 }
 
 // Shift all data up, in two-dimensional array
 void shiftDataVertically() {
 	for (int i = 0; i < displayLines; i++) {
-		strncpy(data[i], data[i + 1], charsPerLine+1);
+		strncpy(data[i], data[i + 1], charsPerLine);
 	}
 }
